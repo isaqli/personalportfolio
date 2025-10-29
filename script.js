1f98f86ddf2588fd6dd71a28ed10a048d64c6f82
isaqli <117958160+isaqli@users.noreply.github.com>
2025-10-29 20:45:03 +0000

    fixed my javascript code on the buttons

--- script.js
+++ script.js
@@ -1,11 +1,21 @@
-// create variables for button and hidden images
-const expandBtn = document.getElementById('expandBtn');
-const hiddenImages = document.querySelectorAll('.hidden');
+function expandButton(buttonId) {
 
-expandBtn.addEventListener('click', () => {
-    hiddenImages.forEach(img => {
-        img.classList.toggle('hidden'); //toggle the visibility of the images
+    // create variables for button and hidden images
+    const expandBtn = document.getElementById(buttonId);
+    const hiddenImages = document.querySelectorAll('.hidden');
+
+    expandBtn.addEventListener('click', () => {
+        hiddenImages.forEach(img => {
+            img.classList.toggle('hidden'); //toggle the visibility of the images
+        });
+
+        if (expandBtn.textContent === 'Show More') {
+            expandBtn.textContent = 'Show Less'    
+        } else {
+            expandBtn.textContent = 'Show More';
+        }
     });
+}
 
-    expandBtn.textContent = expandBtn.textContent === 'Show More' ? 'Show Less' : 'Show More';
-});
+expandButton('expandBtn1');
+expandButton('expandBtn2');
